dfc715991190f96a73ea50edafaad16bb65953d6
Shanshan Su <shanshansu36@gmail.com>
2021-02-17 01:29:33 -0600

    finished arrayOfMultiples function

--- js/edabit-challenges-medium-level.js
+++ js/edabit-challenges-medium-level.js
@@ -109,6 +109,23 @@
     }
 
 
+    /*
+    Creat a function that takes in two numbers as arguments (num, length) and returns an array  of multiples of num until the array length reaches  length.
+
+    Examples
+    arrayOfMultiples(7, 5) ➞ [7, 14, 21, 28, 35]
+    arrayOfMultiples(12, 10) ➞ [12, 24, 36, 48, 60, 72, 84, 96, 108,  120]
+    arrayOfMultiples(17, 6) ➞ [17, 34, 51, 68, 85, 102]
+     */
+
+    function arrayOfMultiples(num, length) {
+        var array = [];
+        for (var i = 1; i <= length; i++) {
+            array.push(num * i);
+        }
+        return array;
+    }
+
 
 
 })();
