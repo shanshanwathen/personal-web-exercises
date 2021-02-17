1c18d05dc103d1b135c7e34de29a9e296a6a9acd
Shanshan Su <shanshansu36@gmail.com>
2021-02-17 02:03:01 -0600

    finished count function

--- js/edabit-challenges-medium-level.js
+++ js/edabit-challenges-medium-level.js
@@ -151,4 +151,23 @@
         }
 
     }
+
+    /*
+    Create a function that counts the integer's number of digits.
+
+    Examples
+    count(318) ➞ 3
+    count(-92563) ➞ 5
+    count(4666) ➞ 4
+    count(-314890) ➞ 6
+    count(654321) ➞ 6
+    count(638476) ➞ 6
+     */
+
+    function count(number) {
+        if (number < 0) {
+            number = number * -1;
+        }
+        return number.toString().length;
+    }
 })();
