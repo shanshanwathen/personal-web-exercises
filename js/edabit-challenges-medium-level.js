0952600f6de31846df63efd75ae1c51715f908bc
Shanshan Su <shanshansu36@gmail.com>
2021-02-17 22:08:46 -0600

    refactored removeABC function

--- js/edabit-challenges-medium-level.js
+++ js/edabit-challenges-medium-level.js
@@ -222,5 +222,33 @@
     }
 
 
+    /*
+    Create a function that takes a number (step) as an argument and returns the amount of boxes in that step of the sequence.
+
+    Step 0: Start with 0
+    Step 1: Add 3
+    Step 2: Subtract 1
+    Repeat Step 1 & 2 ...
+    Examples
+    boxSeq(0) ➞ 0
+
+    boxSeq(1) ➞ 3
+
+    boxSeq(2) ➞ 2
+    Notes
+    Step (the input) is always a positive integer (or zero).
+     */
+
+    function boxSeq(step) {
+        var numberOfBoxes = 0;
+        for (var i = 1; i <= step; i++) {
+            if (i % 2 === 1) {
+                numberOfBoxes = numberOfBoxes + 3;
+            } else {
+                numberOfBoxes = numberOfBoxes -1;
+            }
+        }
+        return numberOfBoxes;
+    }
 
 })();
