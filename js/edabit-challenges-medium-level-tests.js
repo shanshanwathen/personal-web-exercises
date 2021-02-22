9f51797f1044e1904e46a979486d788c76593f02
Shanshan Su <shanshansu36@gmail.com>
2021-02-21 22:01:34 -0600

    working on makeTitle function

--- js/edabit-challenges-medium-level-tests.js
+++ js/edabit-challenges-medium-level-tests.js
@@ -573,3 +573,47 @@ describe("inBox", function() {
         ])).toBe(true);
     });
 })
+
+
+// Capitalize the First Letter of Each Word
+describe("makeTitle", function() {
+    it("should be a defined function", function() {
+        expect(typeof makeTitle).toBe("function");
+    });
+    it("should return a string when executed", function() {
+        expect(typeof makeTitle("")).toBe("string");
+    });
+    it("should return 'I Am A Title' when passed in 'I am a title'", function() {
+        expect(makeTitle("I am a title")).toBe("I Am A Title");
+    });
+    it("should return 'I AM A TITLE' when passed in 'I AM A TITLE'", function() {
+        expect(makeTitle("I AM A TITLE")).toBe("I AM A TITLE");
+    });
+    it("should return 'I AM A TITLE' when passed in 'i aM a tITLE'", function() {
+        expect(makeTitle("i aM a tITLE")).toBe("I AM A TITLE");
+    });
+    it("should return 'The First Letter Of Every Word Is Capitalized' when passed in 'the first letter of every word is capitalized'", function() {
+        expect(makeTitle("the first letter of every word is capitalized")).toBe("The First Letter Of Every Word Is Capitalized");
+    });
+    it("should return 'I LIke Pizza' when passed in 'I Like Pizza'", function() {
+        expect(makeTitle("I Like Pizza")).toBe("I Like Pizza");
+    });
+    it("should return 'Don't Count Your ChiCKens BeFore They HatCh' when passed in 'Don't count your ChiCKens BeFore They HatCh'", function() {
+        expect(makeTitle("Don't count your ChiCKens BeFore They HatCh")).toBe("Don't Count Your ChiCKens BeFore They HatCh");
+    });
+    it("should return 'All Generalizations Are False, Including This One' when passed in 'All generalizations are false, including this one'", function() {
+        expect(makeTitle("All generalizations are false, including this one")).toBe("All Generalizations Are False, Including This One");
+    });
+    it("should return 'Me And My Wife Lived Happily For Twenty Years And Then We Met.' when passed in 'Me and my wife lived happily for twenty years and then we met.'", function() {
+        expect(makeTitle("Me and my wife lived happily for twenty years and then we met.")).toBe("Me And My Wife Lived Happily For Twenty Years And Then We Met.");
+    });
+    it("should return 'There Are No Stupid Questions, Just Stupid People.' when passed in 'There are no stupid questions, just stupid people.'", function() {
+        expect(makeTitle("There are no stupid questions, just stupid people.")).toBe("There Are No Stupid Questions, Just Stupid People.");
+    });
+    it("should return '1f You C4n R34d 7h15, You R34lly N33d 2 G37 L41d' when passed in '1f you c4n r34d 7h15, you r34lly n33d 2 g37 l41d'", function() {
+        expect(makeTitle("1f you c4n r34d 7h15, you r34lly n33d 2 g37 l41d")).toBe("1f You C4n R34d 7h15, You R34lly N33d 2 G37 L41d");
+    });
+    it("should return 'PIZZA PIZZA PIZZA' when passed 'PIZZA PIZZA PIZZA'", function() {
+        expect(makeTitle("PIZZA PIZZA PIZZA")).toBe("PIZZA PIZZA PIZZA");
+    });
+})
--- js/edabit-challenges-medium-level.js
+++ js/edabit-challenges-medium-level.js
@@ -531,3 +531,30 @@ The asterisk may be in the array, however, it must be inside the box, if it exis
 function inBox(arr) {
 
 }
+
+
+/*
+Capitalize the First Letter of Each Word
+
+Create a function that takes a string as an argument and converts the first character of each word to uppercase. Return the newly formatted string.
+
+Examples
+makeTitle("This is a title") ➞ "This Is A Title"
+
+makeTitle("capitalize every word") ➞ "Capitalize Every Word"
+
+makeTitle("I Like Pizza") ➞ "I Like Pizza"
+
+makeTitle("PIZZA PIZZA PIZZA") ➞ "PIZZA PIZZA PIZZA"
+Notes
+You can expect a valid string for each test case.
+Some words may contain more than one uppercase letter (see example #4).
+ */
+
+function makeTitle(str) {
+    var arr = str.split(" ");
+    for (var i = 0; i < arr.length; i++) {
+        arr[i].charAt(0).toUpperCase();
+    }
+    return arr.join(" ");
+}
